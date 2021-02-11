b8b694b2973a294e94a57932c0e753e52360d800
cremirdevio <josephajibodu@gmail.com>
2021-02-11 18:21:09 +0100

    💫 Dijkstra algorithm completed. Node class has been updated to include a visited property

---
+++ src/algorithms/Algos/Algos.java
@@ -0,0 +1,73 @@
+package algorithms.Algos;
+
+import algorithms.Models.Edge;
+import algorithms.Models.Node;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ *
+ * @author Joseph Ajibodu
+ */
+public class Algos {
+
+    public static class Dijkstra {
+
+        public void search(Node start, Node destination) {
+//            Initialize the cost/distance of all nodes to INFINITY
+//            Initialize the start Node to ZERO
+            start.cost = 0;
+
+//            Create a data structure for priortizing Nodes to be visited - Priority Queue
+            PriorityQueue<Node> toVisit = new PriorityQueue<>(new Comparator<Node>() {
+                @Override
+                public int compare(Node o1, Node o2) {
+                    return Double.compare(o1.cost, o2.cost);
+                }
+            });
+
+//            Add the source to the toVisit queue
+            toVisit.add(start);
+
+//            Loop through the Nodes in toVisit till its empty or the destination is VISITED
+            while (!toVisit.isEmpty()) {
+//                Get the highest priority Node (It will be the start Node(since it has a cost of 0) for the first step)
+                Node current = toVisit.poll();
+//                Mark this Node as visited
+                current.visited = true;
+
+//                If the current node is the destination. Stop all operation here
+                if (current.name.equals(destination.name)) {
+                    break;
+                }
+
+//                Loop through the (unvisited) neighbours, perform RELAXATION -> then add to the queue
+                for (Edge edge: current.adjacents) {
+                    Node neighbour = edge.target;
+                    double edgeCost = edge.cost;
+                    
+//                    Skip the neighbouring Node if it has been visited already
+                    if (neighbour.visited) continue;
+                        
+                    double newCost = current.cost + edgeCost;
+//                 RELAXATION: 
+//                  if current_cost + the edge cost < cost of curent Neighbour : Update the cost
+                    if (newCost < neighbour.cost) {
+                        //                        Update the cost
+                        neighbour.cost = newCost;
+//                        Set the parent Node(i.e the previous Node that leads to this Node)
+                        neighbour.parent = current;
+
+//                        This is done to reset the priority queue if necessary
+                        if (toVisit.contains(neighbour)) {
+                            toVisit.remove(neighbour);
+                        }
+
+//                        Add neighbour to the priority queue since it's cost has been updated
+                        toVisit.add(neighbour);
+                    }
+                }
+            }
+        }
+    }
+}
